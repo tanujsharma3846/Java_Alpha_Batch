5d01882b3bb56e3115e681d2ce446ca12c0fabf8
Tanuj Sharma <98101389+tanujsharma3846@users.noreply.github.com>
2023-09-13 19:55:58 +0530

    Add files via upload

---
+++ Divide_and_Conqure/Sorted_Arr.java
@@ -0,0 +1,46 @@
+public class Sorted_Arr {
+
+    public static int search(int arr[], int tar, int si, int ei) {
+        if (si > ei) {
+            return -1;
+        }
+
+        // kaam
+        int mid = si + (ei - si) / 2;
+
+        // best case
+        // case found
+        if (arr[mid] == tar) {
+            return mid;
+        }
+        // line 1
+        if (arr[si] <= arr[mid]) {
+            // case a : left
+            if (arr[si] <= tar && tar <= arr[mid]) {
+                return search(arr, tar, si, mid);
+            }
+            // mid on l2
+            else {
+                // case b
+                return search(arr, tar, mid + 1, ei);
+            }
+        } 
+
+        else {
+            // case c: right
+            if (arr[mid] <= tar && tar <= arr[ei]) {
+                return search(arr, tar, mid + 1, ei);
+            } else {
+                // case d: left
+                return search(arr, tar, si, mid - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 0;
+        int tarIdx = search(arr, target, 0, arr.length - 1);
+        System.out.println(tarIdx);
+    }
+}
